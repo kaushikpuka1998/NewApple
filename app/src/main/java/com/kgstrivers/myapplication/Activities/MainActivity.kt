e1f8e665590d13476af32ae55d86231f51244ef4
kaushik.ghosh <kaushik.ghosh@intrcity.com>
2022-09-16 19:20:35 +0530

    Adding First Commit

---
+++ app/src/main/java/com/kgstrivers/myapplication/Activities/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kgstrivers.myapplication.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kgstrivers.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        
+    }
+}
